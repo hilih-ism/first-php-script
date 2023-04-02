aa3b073ffab99b5c361b326416c5bde6470d77fd
Hildana Shewaneka <112654424+hilih-ism@users.noreply.github.com>
2023-04-02 16:56:13 +0300

    final commit

--- assign.php
+++ assign.php
@@ -1,4 +1,6 @@
 <?php
+//question1
+
 $fc = array(20, 150, 4, $fc2 = array("CS" => 200, "SE" => 350, "IT" => 250, "IS" => 175), 1200, $fc3 = array("FC" => "Bahir Dar"));
 $keys = array_keys($fc3);
 echo "<b>".$keys[0]."</b>"," found at ", "<u>". $fc3['FC'] ."</u>" ,". It has " ,"<b>","<i>". $fc[2] ."</i>","</b>", " departments, ", 
@@ -9,5 +11,24 @@ foreach(array_keys($fc2) as $key) {
   if(is_string($key)) {
     echo $key . ", ";
   }
+}
+
+//question2
+
+$dept = array("CS","SEng","IT","IS");
+
+foreach($dept as $dept)
+{
+	echo "<ul>","<li>". $dept ."</li>","</ul>";
+}
+
+//question3
+
+$string = "Human beings have an innate desire to be listened, but only few are good listeners. 
+So, if you listen to someone attentively, you'll flatter them.";
+
+$words = explode(" ", $string);
+for ($i = 0;$i<10;$i++){
+	echo $words[$i]. " ";
 }
 ?>
